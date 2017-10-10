bd96302acc6dd4a73fb7e3b727a19220a4d2de8c
zeeshanpro <32648852+zeeshanpro@users.noreply.github.com>
2017-10-10 06:22:49 +0500

    pull repo1 and then commit

--- product page.php
+++ product page.php
@@ -6,9 +6,9 @@
  * Time: 5:24 AM
  */
 
-<<<<<<< HEAD
+
 echo "this is a product page by clone2 TestRep1";
-=======
+
 
 echo "this is a product page by clone1 TestRep";
->>>>>>> be5e2d411bf364fb0c5c35a8bc56036b88295d65
+
